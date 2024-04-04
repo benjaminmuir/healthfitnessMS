e8b70f9f08d326d386107136c6ff94c92df654e3
benjaminmuir <bmuir052@gmail.com>
2024-04-03 23:28:53 -0400

    Added new members functionality, left is dashboard and sheculdes

--- finalprojecttest.py
+++ finalprojecttest.py
@@ -36,43 +36,50 @@ Are you a Member (1), Trainer (2), or Adminstration (3)? (Quit at any time with 
         case "1":
             existing = str(input("\nDo you have an existing account? (Y or N)\n"))
 
+            #input check
             while existing not in ["Y","N","y","n","yes","no","YES","NO"]:
                 print("Please enter a valid answer to the question.\n")
                 existing = str(input("\nDo you have an existing account? (Y or N)\n"))
 
-            if existing in ["n", "N", "no", "NO"]: # they are looking to make a new account
+            # they are looking to make a new account
+            if existing in ["n", "N", "no", "NO"]: 
                 usernameId = str(input("\nPlease sign in with your username:\n"))
                 userPassWord = str(input("Password:\n"))
+                #make new data in the member table and set id username and password for now
                 try:
-                    #make new data in the member table and set id username and password for now
+                    #set the data for username and password only
                     cur.execute("INSERT INTO member (username, password) VALUES (%s, %s)",
-                                (usernameId,userPassWord))
+                            (usernameId,userPassWord))
                     
-                    #get memberID from this member and set to a variable
+                    #get the memberID of the new member
                     cur.execute("SELECT memberID FROM member WHERE username = %s AND password = %s",
-                                (usernameId,userPassWord)) 
-                    memberID = str(cur.fetchone()[0]) #get only the ID 
+                            (usernameId,userPassWord))
+                    memberID = str(cur.fetchone()[0])
                     print(f"\nNew member {usernameId} added!\n")
+                    db.commit()
+                    return usernameId, userPassWord, memberID
+                
                 except:
                     print("Either username or password is the same as an existing member.")
-                    memberID = ""
-                    userLogin()
+                    return False
 
             #if the member has an account, simply fetch the relevant data
-            elif existing in ["y", "Y", "yes", "YES"]:
+            if existing in ["y", "Y", "yes", "YES"]:
                 usernameId = str(input("\nPlease sign in with your username:\n"))
                 userPassWord = str(input("Password:\n"))
                 #get memberID from this member and set to a variable
-                cur.execute("SELECT memberID FROM member WHERE username = %s AND password = %s",
-                            (usernameId,userPassWord)) 
+                
                 #check for non-existing member
                 try: 
+                    cur.execute("SELECT memberID FROM member WHERE username = %s AND password = %s",
+                            (usernameId,userPassWord)) 
                     memberID = str(cur.fetchone()[0])
                     print(f"\nWelcome back {usernameId}!\n")
+                    db.commit()
+                    return usernameId, userPassWord, memberID
                 except:
                     print("No member found with that username and password.")
-                    memberID = "" #set to empty string to avoid errors
-                    userLogin()
+                    return False
                 
         case "2":
             print("I will implement this later")
@@ -93,23 +100,35 @@ Are you a Member (1), Trainer (2), or Adminstration (3)? (Quit at any time with 
         case _:
             while logInType not in {"1","2","3"}:
                 print("Please enter a number from 1 to 3.")
-                userLogin()
+                return False
+
     return usernameId, userPassWord, memberID
 
-def updatePersonalInfo(columnName,memberID):
-    cur.execute("UPDATE {columnName} SET {columnName} = %s WHERE memberID = %s",(columnName,memberID))
 
+
+"""
+Prints the profile menu for any member. 
+Has options:
+    Update Personal Info:
+        Will update age,fname,lname,email,height,weight 
+    Update Goals:
+        Used to update weightGoal, lapTime (and others)
+    Update Health Metrics:
+        Used to update bloodoxygen, averageHeartRate 
+Params: memberID
+    Takes current memberID of the member that is currently logged on. 
+"""
 def profileMenu(memberID):
     col_names = []
     data_rows = []
     choice = input("""\n
----------------------------------------------------------------------------------------------------
+--------------------------------
 Profile Menu:
 (1) Update Personal Info
-(2) Update Goals
+(2) Update Fitness Goals
 (3) Update Health Metrics
-(4) View Profile
----------------------------------------------------------------------------------------------------\n""")
+(4) View Profile Info and Health Statistics/Metrics
+-------------------------------\n""")
     match choice:
         case "1":
             print("You have chosen to update your personal info!\n")
@@ -122,7 +141,7 @@ Profile Menu:
             print("Here is your current personal info:\n")
             for data in col_names:
                 print(f"{data}: {data_rows[0][col_names.index(data)]}")
-            columnUpdate = int(input("""
+            columnUpdate = (input("""
 Which column would you like to update?
 (0) First Name
 (1) Last Name
@@ -133,20 +152,64 @@ Which column would you like to update?
 \n"""))
             dataUpdate = input("Please enter the new data:\n")
             for data in col_names:
-                print(str(col_names.index(data)) + " " + data)
-                if columnUpdate == col_names.index(data):
-                    cur.execute(f"UPDATE member SET {data} = %s WHERE memberID = %s",(dataUpdate,memberID))
-                    print("Data updated successfully!")
+                if int(columnUpdate) == col_names.index(data):
+                    if data == "age" or data == "height" or data == "weight":
+                        cur.execute(f"UPDATE member SET {data} = %s WHERE memberID = %s",(int(dataUpdate),memberID))
+                        print(f"Updated {data} to {dataUpdate}")
+                        db.commit()
+                    else:
+                        cur.execute(f"UPDATE member SET {data} = %s WHERE memberID = %s",(dataUpdate,memberID))
+                        print(f"Updated {data} to {dataUpdate}")
+                        db.commit()
+                    
             # cur.execute(f"UPDATE member SET {col_names[columnUpdate]} = %s WHERE memberID = %s",(dataUpdate,memberID))
         case "2":
-            print("You have chosen to update your goals!")
+            print("You have chosen to update your goals!\n")
+            weightGoal = input("Please enter your new weight goal (in lbs):\n")
+            lapTime = input("Please enter your new lap time goal (in minutes.seconds):\n")
+            deadliftMax = input("Please enter your new deadlift max (in lbs):\n")
+            squatMax = input("Please enter your new squat max (in lbs):\n")
+            benchMax = input("Please enter your new bench max (in lbs):\n")
+            swimTime = input("Please enter your new swim time (in minutes.seconds):\n")
+            cur.execute("UPDATE member SET weightGoal = %s, lapTime = %s, deadliftMax = %s, squatMax = %s, benchMax = %s, swimTime = %s WHERE memberID = %s",
+                        (weightGoal,lapTime,deadliftMax,squatMax,benchMax,swimTime,memberID))
+            print("Updated your goals!")
+            db.commit()
             #updateGoals(memberID)
         case "3":
-            print("You have chosen to update your health metrics!")
+            print("You have chosen to update your health metrics!\n")
+            bmi = input("Please enter your new BMI level:\n")
+            averageHeartRate = input("Please enter your new resting heart rate:\n")
+            
+            cur.execute("UPDATE member SET bmi = %s, restingHeartRate = %s WHERE memberID = %s",
+                        (bmi,averageHeartRate,memberID))
+            print("Updated your health metrics!")
+            db.commit()
             #updateHealthMetrics(memberID)
         case "4":
-            print("You have chosen to view your profile!")
-            #viewProfile(memberID)
+            #print profile info, goals, and health metrics in seperate sections all titled
+            print("Here is your profile information:\n")
+            cur.execute("SELECT fName,lName,email,height,weight,age FROM member WHERE memberID = %s",(memberID,))
+            col_names = ([desc[0] for desc in cur.description])
+            for row in cur:
+                data_rows.append(row)
+            for data in col_names:
+                print(f"{data}: {data_rows[0][col_names.index(data)]}")
+            print("\nHere are your fitness goals:\n")
+            cur.execute("SELECT weightGoal,lapTime,deadliftMax,squatMax,benchMax,swimTime FROM member WHERE memberID = %s",(memberID,))
+            col_names = ([desc[0] for desc in cur.description])
+            for row in cur:
+                data_rows.append(row)
+            for data in col_names:
+                print(f"{data}: {data_rows[1][col_names.index(data)]}")
+            print("\nHere are your health metrics:\n")
+            cur.execute("SELECT bmi,restingHeartRate FROM member WHERE memberID = %s",(memberID,))
+            col_names = ([desc[0] for desc in cur.description])
+            for row in cur:
+                data_rows.append(row)
+            for data in col_names:
+                print(f"{data}: {data_rows[2][col_names.index(data)]}")
+            db.commit()
         case _:
             while choice not in {"1","2","3","4"}:
                 print("Please enter a number from 1 to 4.")
@@ -160,34 +223,30 @@ def scheduleMenu():
 
 def main():
     userType = userLogin()
+    #ensure we are storing the memberID of the user if anything goes wrong
+    while userType == False:
+        userType = userLogin()
+
+    #now we ask for menu options 
     userMenuChoice = userMenu()
-    #get the memberID from the user who logged in
-    #to reference in the other functions
-    currentMemberID = userType[2]
-    match userMenuChoice:
-        case "1":
-            print("You have chosen the profile menu!")
-            profileMenu(currentMemberID) 
-        case "2":
-            print("You have chosen the dashboard menu!")
-            dashBoardDisplay() 
-        case "3":
-            print("You have chosen the schedule menu!")
-            scheduleMenu() 
-        case "q":
-            print("Exiting the system.")
-            db.commit()
-            cur.close()
-            db.close()
-            quit()
-        case _:
-            while userMenuChoice not in {"1","2","3","q"}:
-                print("Please enter a number from 1 to 3.")
-                userMenuChoice = userMenu()
-    #ensure we commit cursor changes and close db
-    cur.close()
+    while userMenuChoice not in {"q","Q","quit","Quit"}:
+        #keep updating current member ID that is logged in
+        currentMemberID = userType[2]
+        if userMenuChoice == "1":
+            profileMenu(currentMemberID)
+        elif userMenuChoice == "2":
+            dashBoardDisplay()
+        elif userMenuChoice == "3":
+            scheduleMenu()
+        db.commit()
+        userMenuChoice = userMenu()
+
+
+    print("Exiting the system.")
     db.commit()
+    cur.close()
     db.close()
+    quit()
 
 if __name__ == "__main__":
     main()
