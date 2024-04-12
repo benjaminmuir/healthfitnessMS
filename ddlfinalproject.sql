30dd19bba5c3d3efb53aabfda9b67a875f809207
benjaminmuir <bmuir052@gmail.com>
2024-04-12 02:44:30 -0400

    Member display/schedule, trainer display/schedule, bills, sessions, TODO: rooms, classes, admin

--- ddlfinalproject.sql
+++ ddlfinalproject.sql
@@ -2,14 +2,14 @@ create table trainer
     (trainerID      serial UNIQUE not null, 
      username       varchar(255) UNIQUE not null, 
      password       varchar(255) UNIQUE not null,
-     name           varchar(255),
+     name           varchar(255) not null,
      primary key (trainerID)
     );
 	
 create table member 
     (memberID      serial UNIQUE not null, 
-     username       varchar(18) UNIQUE not null, 
-     password       varchar(18) UNIQUE not null,
+     username       varchar(255) UNIQUE not null, 
+     password       varchar(255) UNIQUE not null,
      fName           varchar(255) ,
      lName          varchar(255),
      email          varchar(255) ,
@@ -28,42 +28,73 @@ create table member
      primary key (memberID)
     );
 
+create table exerciseRoutine 
+    (routineID serial UNIQUE not null,
+     name       varchar(255),
+     memberID integer not null,
+     repetitions integer CHECK (repetitions > 0),
+     primary key (routineID),
+     foreign key (memberID) references member (memberID)
+        on delete cascade       
+    );
+
+create table class 
+    (CID        serial UNIQUE not null,
+     day        varchar CHECK (day IN ('Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday')),
+     timeStart  time CHECK (timeStart >= '00:00:00' and timeStart <= '23:59:59' and timeStart < timeEnd ),
+     timeEnd    time CHECK (timeEnd >= '00:00:00' and timeEnd <= '23:59:59' and timeEnd > timeStart ), 
+     classExercise  varchar(255),
+     memberID integer,
+     trainerID integer UNIQUE,
+     primary key (CID),
+     foreign key (memberID) references member (memberID)
+        on delete cascade,
+     foreign key (trainerID) references trainer (trainerID)
+    );
+
 create table room
     (roomID serial UNIQUE not null,
      eventName varchar(255),
-     eventStart time CHECK (timeStart > '00:00:00' and timeStart < '24:00:00'),
-     eventEnd time CHECK (timeStart > '00:00:00' and timeStart < '24:00:00'),
+     eventStart time CHECK (eventStart >= '00:00:00' and eventStart <= '23:59:59' and eventStart < eventEnd),
+     eventEnd time CHECK (eventEnd >= '00:00:00' and eventEnd <= '23:59:59' and eventEnd > eventStart),
      capacity integer CHECK (capacity > 0),
      CID integer,
-     primary key (roomID)
+     primary key (roomID),
      foreign key (CID) references class (CID)
         on delete set null
     );    
-	
+
+create table payment
+    (billID serial UNIQUE not null,
+     amount numeric CHECK (amount > 0) not null, 
+     memberID  integer not null, 
+     paid   boolean not null,
+     primary key (billID),
+     foreign key (memberID) references member (memberID)
+        on delete cascade
+    );
+
 create table session
-    (SID        integer UNIQUE not null,
+    (SID        serial UNIQUE not null,
      trainerID  integer not null,
      memberID   integer not null,
-     day        integer CHECK (day > 0 and day < 32), 
-     timeStart  time CHECK (timeStart > '00:00:00' and timeStart < '24:00:00'),
-     timeEnd    time CHECK (timeEnd > '00:00:00' and timeEnd < '24:00:00'), 
-     roomID     integer, 
+     day        varchar CHECK (day IN ('Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday')), 
+     timeStart  time CHECK (timeStart >= '00:00:00' and timeStart <= '23:59:59' and timeStart < timeEnd),
+     timeEnd    time CHECK (timeEnd >= '00:00:00' and timeEnd <= '23:59:59' and timeEnd > timeStart), 
      primary key (SID, trainerID),
      foreign key (trainerID) references trainer (trainerID)
         on delete cascade,
      foreign key (memberID) references member (memberID)
         on delete cascade,
-     foreign key (roomID) references room (roomID)
-        on delete cascade
     );	
 
 create table schedule 
     (trainerID integer not null, 
      day        varchar(9) CHECK (day IN ('Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday')),
-     timeStart  time CHECK (timeStart > '00:00:00' and timeStart < '24:00:00'),
-     timeEnd    time CHECK (timeEnd > '00:00:00' and timeEnd < '24:00:00'),
+     timeStart  time CHECK (timeStart >= '00:00:00' and timeStart <= '23:59:59' and timeStart < timeEnd),
+     timeEnd    time CHECK (timeEnd >= '00:00:00' and timeEnd <= '23:59:59' and timeEnd > timeStart),
      foreign key (trainerID) references trainer (trainerID)
-        on delete set cascade
+        on delete cascade
     );    
 
 
@@ -87,41 +118,12 @@ create table registers
         on delete set null
     );
     
-create table exerciseRoutine 
-    (routineID serial UNIQUE not null,
-     name       varchar(255),
-     memberID integer not null,
-     repetitions integer CHECK (repetitions > 0),
-     primary key (routineID),
-     foreign key (memberID) references member (memberID)
-        on delete cascade       
-    );
-
-create table class 
-    (CID        integer UNIQUE not null,
-     day        date,
-     timeStart  time CHECK (timeStart > '00:00:00' and timeStart < '24:00:00'),
-     timeEnd    time CHECK (timeEnd > '00:00:00' and timeEnd < '24:00:00'), 
-     classExercise  integer UNIQUE,
-     memberID integer,
-     trainerID integer UNIQUE,
-     roomID     integer,
-     primary key (CID),
-     foreign key (classExercise) references exerciseRoutine (routineID)
-        on delete set null,
-     foreign key (roomID) references room (roomID)
-        on delete cascade,
-     foreign key (memberID) references member (memberID)
-        on delete cascade,
-     foreign key (trainerID) references trainer (trainerID)
-    );
-
 create table memberClass
     (CID        integer,
      memberID        integer,
      primary key (CID, memberID),
      foreign key (CID) references class (CID)
-        on delete set null
-    foreign key (memberID) references member (memberID)
+        on delete set null,
+     foreign key (memberID) references member (memberID)
     );
     
